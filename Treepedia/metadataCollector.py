288513c9649a2c1aab33967a5e1490d38d308960
axeln78 <axeln78@gmail.com>
2021-06-17 10:15:03 +0200

    Minor changes

--- Treepedia/metadataCollector.py
+++ Treepedia/metadataCollector.py
@@ -19,10 +19,12 @@ def GSVpanoMetadataCollector(samplesFeatureClass,num,ouputTextFolder):
     import urllib.request, urllib.parse, urllib.error,urllib.request,urllib.error,urllib.parse
     import xmltodict
     import io
+    import math
     from osgeo import ogr, osr
     import time
     import os,os.path
     import json
+    import streetview
 
     if not os.path.exists(ouputTextFolder):
         os.makedirs(ouputTextFolder)
@@ -63,6 +65,8 @@ def GSVpanoMetadataCollector(samplesFeatureClass,num,ouputTextFolder):
 
         time.sleep(1)
 
+        key = get_keys()  #Input Your Key here
+
         with open(ouputGSVinfoFile, 'w') as panoInfoText:
             # process num feature each time
             for i in range(start, end):
@@ -72,12 +76,11 @@ def GSVpanoMetadataCollector(samplesFeatureClass,num,ouputTextFolder):
                 # trasform the current projection of input shapefile to WGS84
                 #WGS84 is Earth centered, earth fixed terrestrial ref system
                 geom.Transform(transform)
-                lon = geom.GetX()
-                lat = geom.GetY()
-                key = get_keys()  #Input Your Key here
+                lat = geom.GetX()
+                lon = geom.GetY()
 
                 # get the meta data of panoramas
-                urlAddress = 'https://maps.googleapis.com/maps/api/streetview/metadata?location=%s,%s&key=%s'%(lon,lat,key)
+                urlAddress = 'https://maps.googleapis.com/maps/api/streetview/metadata?location=%s,%s&key=%s'%(lat,lon,key)
 
                 time.sleep(0.01)
                 # the output result of the meta data is a json object
@@ -86,8 +89,8 @@ def GSVpanoMetadataCollector(samplesFeatureClass,num,ouputTextFolder):
                 data = json.loads(metaData)
                 #print(data)
 
-                # in case there is not panorama in the site, therefore, continue
-                if data['status']=='ZERO_RESULTS':
+                # in case there is not panorama in the site, continue
+                if data['status'] != 'OK':
                     continue
                 else:
                     panoDate, panoId, panoLat, panoLon = getPanoItems(data)
@@ -100,7 +103,7 @@ def GSVpanoMetadataCollector(samplesFeatureClass,num,ouputTextFolder):
 
 def getPanoItems(data):
     # get the meta data of the panorama
-    panoDate = data['date']
+    panoDate = data.get('date') # Sometimes the date is not available exception
     panoId = data['pano_id']
     panoLat = data['location']['lat']
     panoLon = data['location']['lng']
@@ -108,7 +111,7 @@ def getPanoItems(data):
 
 def get_keys():
     # Function to get the keys from main keys file
-    os.chdir("../")
+    os.chdir("./")
     key_file = os.path.join(os.getcwd(), 'keys.txt')
 
     lines = open(key_file,"r")
@@ -118,7 +121,7 @@ def get_keys():
         keylist.append(key)
     lines.close()
 
-    print ('The key list is:=============', keylist)
+    #print ('The key list is:=============', keylist)
     return keylist[0]
 
 # ------------Main Function -------------------
@@ -126,7 +129,7 @@ if __name__ == "__main__":
     import os, os.path
 
     root = '../spatial-data'
-    inputShp = os.path.join(root,'Cambridge20m.shp')
+    inputShp = os.path.join(root,'GLSG_small_out.shp')
     outputTxt = root
 
     GSVpanoMetadataCollector(inputShp,1000,outputTxt)
