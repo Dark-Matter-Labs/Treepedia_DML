863cfe9cd190437d3cac39e65a26506359580dc0
axeln78 <axeln78@gmail.com>
2021-06-24 09:42:27 +0200

    Added Config file3

---
+++ Treepedia/config.py
@@ -0,0 +1,20 @@
+shapefile = {
+    'area': 'Knightswood',
+    'input': 'Knightswood_planet_osm_line_lines.shp',
+    'dotted': 'Knightswood_out.shp'
+    }
+
+
+GVIfile = {
+    'images':  './imgs_Knightswood/',
+    'shapefile': 'GVI_Knightswood.shp',
+    'data': 'greenViewRes'
+    }
+
+greenmonth = ['04','05','06','07','08','09']
+
+gcloud_key = 'G3tUr0wnAp1K3y'
+
+POINT_DIST = 50
+
+root_dir = '../spatial-data'
